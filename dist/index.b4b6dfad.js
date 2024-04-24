bc7634326c35d96536679cb23350883bcdec2662
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-04-24 08:17:45 +0200

    Parcel built after created MainView

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -2953,23 +2953,15 @@ $parcel$ReactRefreshHelpers$98a3.prelude(module);
 try {
 var _jsxDevRuntime = require("react/jsx-dev-runtime");
 var _client = require("react-dom/client");
+var _mainView = require("./components/main-view/main-view");
 // Import statement to indicate to bundle ./index.scss
 var _indexScss = require("./index.scss");
 // Main component (will eventually use all the others)
 const MyFlixApplication = ()=>{
-    return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
-        className: "my-flix",
-        children: /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
-            children: "Good morning"
-        }, void 0, false, {
-            fileName: "src/index.jsx",
-            lineNumber: 10,
-            columnNumber: 7
-        }, undefined)
-    }, void 0, false, {
+    return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _mainView.MainView), {}, void 0, false, {
         fileName: "src/index.jsx",
         lineNumber: 9,
-        columnNumber: 5
+        columnNumber: 10
     }, undefined);
 };
 _c = MyFlixApplication;
@@ -2979,7 +2971,7 @@ const root = (0, _client.createRoot)(container);
 // Tells React to render the app in the root DOM element
 root.render(/*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)(MyFlixApplication, {}, void 0, false, {
     fileName: "src/index.jsx",
-    lineNumber: 20,
+    lineNumber: 17,
     columnNumber: 13
 }, undefined));
 var _c;
@@ -2990,7 +2982,7 @@ $RefreshReg$(_c, "MyFlixApplication");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","react-dom/client":"lOjBx","./index.scss":"lJZlQ","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"fDiVC"}],"iTorj":[function(require,module,exports) {
+},{"react/jsx-dev-runtime":"iTorj","react-dom/client":"lOjBx","./index.scss":"lJZlQ","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"fDiVC","./components/main-view/main-view":"4gflv"}],"iTorj":[function(require,module,exports) {
 "use strict";
 module.exports = require("ee51401569654d91");
 
@@ -27315,6 +27307,188 @@ function registerExportsForReactRefresh(module1) {
     }
 }
 
-},{"2f1dfff7d265aa49":"8QnHs"}]},["1GoAO","dAPVv","d8Dch"], "d8Dch", "parcelRequireaec4")
+},{"2f1dfff7d265aa49":"8QnHs"}],"4gflv":[function(require,module,exports) {
+var $parcel$ReactRefreshHelpers$f7a6 = require("@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js");
+var prevRefreshReg = window.$RefreshReg$;
+var prevRefreshSig = window.$RefreshSig$;
+$parcel$ReactRefreshHelpers$f7a6.prelude(module);
+
+try {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "MainView", ()=>MainView);
+var _jsxDevRuntime = require("react/jsx-dev-runtime");
+var _react = require("react");
+var _movieCard = require("../movie-card/movie-card");
+var _s = $RefreshSig$();
+const MainView = ()=>{
+    _s();
+    const [movies, setMovies] = (0, _react.useState)([
+        {
+            "Genre": {
+                "Name": "Action",
+                "Description": "Action films involve physical feats, chases, fights, and battles."
+            },
+            "Director": {
+                "Name": "Stanley Kubrick",
+                "Bio": "Stanley Kubrick was an American film director, screenwriter, and producer. He is considered one of the greatest and most influential directors in cinematic history. Kubrick's films are known for their technical precision, innovative use of music, and thought-provoking themes. Some of his most famous works include 2001: A Space Odyssey, A Clockwork Orange, and The Shining.",
+                "Birth": "1928-07-26T00:00:00.000Z",
+                "Death": "1999-03-07T00:00:00.000Z"
+            },
+            "Actors": [],
+            "_id": "660cf4f4a8e808e3b590a916",
+            "Title": "2001: A Space Odyssey",
+            "Description": "Humanity finds a mysterious, obviously artificial object buried beneath the Lunar surface and, with the intelligent computer H.A.L. 9000, sets off on a quest.",
+            "ImagePath": "2001aspaceodyssey.png",
+            "Featured": true
+        },
+        {
+            "Genre": {
+                "Name": "Action",
+                "Description": "Action films involve physical feats, chases, fights, and battles."
+            },
+            "Director": {
+                "Name": "Christopher Nolan",
+                "Bio": "Christopher Edward Nolan is a British-American film director, producer, and screenwriter. He is known for his distinctive style of storytelling, often involving nonlinear narratives and exploring themes of memory, identity, and time. Nolan's films, such as Inception and The Dark Knight trilogy, have been both critically acclaimed and commercially successful.",
+                "Birth": "1970-07-30T00:00:00.000Z",
+                "Death": null
+            },
+            "Actors": [],
+            "_id": "660cf026a8e808e3b590a90f",
+            "Title": "The Dark Knight",
+            "Description": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+            "ImagePath": "thedarkknight.png",
+            "Featured": false
+        },
+        {
+            "Genre": {
+                "Name": "Drama",
+                "Description": "Drama films are intended to portray realistic characters, settings, life situations, and stories."
+            },
+            "Director": {
+                "Name": "James Cameron",
+                "Bio": "James Francis Cameron is a Canadian film director, producer, and screenwriter. He is known for his groundbreaking work in the science fiction genre, particularly with the films The Terminator, Aliens, and Avatar. Cameron's films are often characterized by their innovative special effects and immersive storytelling.",
+                "Birth": "1954-08-16T00:00:00.000Z",
+                "Death": null
+            },
+            "Actors": [],
+            "_id": "660cf110a8e808e3b590a911",
+            "Title": "The Shawshank Redemption",
+            "Description": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+            "ImagePath": "shawshankredemption.png",
+            "Featured": true
+        },
+        {
+            "Genre": {
+                "Name": "Adventure",
+                "Description": "Adventure films typically involve characters embarking on a journey or quest, often featuring excitement, exploration, and risk-taking."
+            },
+            "Director": {
+                "Name": "Billy Wilder",
+                "Bio": "Billy Wilder was an Austrian-American film director, screenwriter, and producer. He is regarded as one of the most brilliant and versatile filmmakers of the Hollywood Golden Age. Wilder's films are known for their sharp wit, cynical humor, and incisive commentary on society. Some of his most acclaimed works include Sunset Boulevard, Some Like It Hot, and The Apartment.",
+                "Birth": "1906-06-22T00:00:00.000Z",
+                "Death": "2002-03-27T00:00:00.000Z"
+            },
+            "Actors": [],
+            "_id": "660cf441a8e808e3b590a915",
+            "Title": "Aliens",
+            "Description": "Fifty-seven years after surviving an apocalyptic attack aboard her space vessel by merciless space creatures, Officer Ripley awakens from hyper-sleep and tries to warn anyone who will listen about the predators.",
+            "ImagePath": "aliens.png",
+            "Featured": true
+        }
+    ]);
+    if (movies.length === 0) return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
+        children: "There is no movie to display!"
+    }, void 0, false, {
+        fileName: "src/components/main-view/main-view.jsx",
+        lineNumber: 81,
+        columnNumber: 12
+    }, undefined);
+    else return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
+        children: movies.map((movie)=>{
+            return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _movieCard.MovieCard), {
+                movieData: movie
+            }, movie._id, false, {
+                fileName: "src/components/main-view/main-view.jsx",
+                lineNumber: 87,
+                columnNumber: 13
+            }, undefined);
+        })
+    }, void 0, false, {
+        fileName: "src/components/main-view/main-view.jsx",
+        lineNumber: 84,
+        columnNumber: 7
+    }, undefined);
+};
+_s(MainView, "vkzn2EO1e09CBStK+jj2QEhiU4E=");
+_c = MainView;
+var _c;
+$RefreshReg$(_c, "MainView");
+
+  $parcel$ReactRefreshHelpers$f7a6.postlude(module);
+} finally {
+  window.$RefreshReg$ = prevRefreshReg;
+  window.$RefreshSig$ = prevRefreshSig;
+}
+},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","../movie-card/movie-card":"bwuIu","@parcel/transformer-js/src/esmodule-helpers.js":"fD7H8","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"fDiVC"}],"bwuIu":[function(require,module,exports) {
+var $parcel$ReactRefreshHelpers$67b2 = require("@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js");
+var prevRefreshReg = window.$RefreshReg$;
+var prevRefreshSig = window.$RefreshSig$;
+$parcel$ReactRefreshHelpers$67b2.prelude(module);
+
+try {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "MovieCard", ()=>MovieCard);
+var _jsxDevRuntime = require("react/jsx-dev-runtime");
+const MovieCard = ({ movieData })=>{
+    return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
+        children: movieData.Title
+    }, void 0, false, {
+        fileName: "src/components/movie-card/movie-card.jsx",
+        lineNumber: 3,
+        columnNumber: 5
+    }, undefined);
+};
+_c = MovieCard;
+var _c;
+$RefreshReg$(_c, "MovieCard");
+
+  $parcel$ReactRefreshHelpers$67b2.postlude(module);
+} finally {
+  window.$RefreshReg$ = prevRefreshReg;
+  window.$RefreshSig$ = prevRefreshSig;
+}
+},{"react/jsx-dev-runtime":"iTorj","@parcel/transformer-js/src/esmodule-helpers.js":"fD7H8","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"fDiVC"}],"fD7H8":[function(require,module,exports) {
+exports.interopDefault = function(a) {
+    return a && a.__esModule ? a : {
+        default: a
+    };
+};
+exports.defineInteropFlag = function(a) {
+    Object.defineProperty(a, "__esModule", {
+        value: true
+    });
+};
+exports.exportAll = function(source, dest) {
+    Object.keys(source).forEach(function(key) {
+        if (key === "default" || key === "__esModule" || Object.prototype.hasOwnProperty.call(dest, key)) return;
+        Object.defineProperty(dest, key, {
+            enumerable: true,
+            get: function() {
+                return source[key];
+            }
+        });
+    });
+    return dest;
+};
+exports.export = function(dest, destName, get) {
+    Object.defineProperty(dest, destName, {
+        enumerable: true,
+        get: get
+    });
+};
+
+},{}]},["1GoAO","dAPVv","d8Dch"], "d8Dch", "parcelRequireaec4")
 
 //# sourceMappingURL=index.b4b6dfad.js.map
