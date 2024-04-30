edff691cc0474afe6da44c766d86ee55fb6af913
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-04-30 15:25:57 +0200

    Init login-view

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,9 @@
+export const LoginView = () => {
+  return (
+    <form>
+      <label>Username:<input /></label>
+      <label>Password:<input /></label>
+      <button type="Submit">Submit</button>
+    </form>
+  )
+}
